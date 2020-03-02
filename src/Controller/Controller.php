63ae9367546186280259cf44c20464a664fcda70
Konrad <k.lyzwa7@gmail.com>
2020-03-02 10:17:07 +0100

    initial commit

---
+++ src/Controller/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Teik\Controller;
+
+use Teik\Traits\RandomToken;
+use Timber\Timber;
+
+class Controller
+{
+    use RandomToken;
+    /**
+     * Hook into theme
+     */
+    public function hook()
+    { }
+}
