0aa4ce7ae546149e3931b371d40118f31c21dc18
Konrad <k.lyzwa7@gmail.com>
2020-03-03 09:29:31 +0100

    create about block

--- src/Blocks/AboutContact.php
+++ src/Blocks/AboutContact.php
@@ -11,4 +11,11 @@ class AboutContact extends AbstractBlock
     public $name = 'about-contact';
 
     public $title = 'O produkcie + kontakt';
+
+    public function getBlockData(array $data = []): array
+    {
+        $data = parent::getBlockData();
+        $data['contact_data'] = get_field('contact_data', 'options');
+        return $data;
+    }
 }
